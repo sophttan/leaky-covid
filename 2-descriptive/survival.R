ca064ee6247e185987157f3ae0f0b93dfd70d626
Sophia Tan <sophiatan@berkeley.edu>
2024-01-09 11:04:15 -0800

    censoring csv + distributions of time til censoring

--- 2-descriptive/survival.R
+++ 2-descriptive/survival.R
@@ -24,40 +24,47 @@ inf <- inf %>%
 
 
 test <- function(subvariant) {
-  first_day <- c("2021-12-15", "2022-05-15", "2022-08-15", "2022-12-15")
-  last_day <- c("2022-05-14", "2022-08-14", "2022-12-14", "2023-03-14")
+  first_day <- c("2021-12-15", "2022-05-15", "2022-08-15", "2022-12-15") %>% as.Date()
+  last_day <- c("2022-05-14", "2022-08-14", "2022-12-14", "2023-03-14") %>% as.Date()
   
   resident_movement_subvar <- resident_movement %>% filter(first <= first_day[subvariant] & last >= first_day[subvariant])
 
-  exclude_residents_initial <- (inf %>% filter(any(as.Date(first_day[subvariant])-inf.Day<=90 & as.Date(first_day[subvariant])-inf.Day>0)))$ResidentId %>% unique()
+  exclude_residents_initial <- (inf %>% filter(any(first_day[subvariant]-inf.Day<=90 & first_day[subvariant]-inf.Day>0)))$ResidentId %>% unique()
   
   resident_movement_subvar <- resident_movement_subvar %>% filter(!ResidentId %in% exclude_residents_initial)
   
   resident_movement_subvar <- resident_movement_subvar %>% mutate(first_adj=first_day[subvariant])
   
   resident_movement_subvar <- resident_movement_subvar %>% 
-    left_join(inf %>% filter(inf.Day >= first_day[subvariant] & inf.Day <= last_day[subvariant])) %>% 
+    left_join(inf %>% 
+                filter(inf.Day >= first_day[subvariant] & inf.Day <= last_day[subvariant])) %>% 
     filter(n()==1 | inf.Day==first(inf.Day)) 
   
   resident_movement_subvar <- resident_movement_subvar %>%
-    left_join(vacc %>% select(ResidentId, vacc.Day, num_dose) %>% filter(vacc.Day >= first_day[subvariant] & vacc.Day <= last_day[subvariant])) %>%
+    left_join(vacc %>% 
+                select(ResidentId, vacc.Day, num_dose) %>% 
+                filter(vacc.Day >= first_day[subvariant] & vacc.Day <= last_day[subvariant])) %>%
     filter(n()==1 | vacc.Day==last(vacc.Day))
   
   resident_movement_subvar <- resident_movement_subvar %>% 
     mutate(vacc.Day = if_else(vacc.Day <= first_day[subvariant], NA, vacc.Day),
            inf.Day = if_else(inf.Day <= first_day[subvariant], NA, inf.Day),
-           earliest=pmin(as.Date(last_day[subvariant]), last, vacc.Day,na.rm=T),
-           earliest_novacc=pmin(as.Date(last_day[subvariant]), last,na.rm=T),
-           censored = case_when(earliest==as.Date(last_day[subvariant])~"end_period",
+           earliest=pmin(last_day[subvariant], last, vacc.Day,na.rm=T),
+           earliest_novacc=pmin(last_day[subvariant], last,na.rm=T),
+           censored = case_when(earliest==last_day[subvariant]~"end_period",
                                 earliest==vacc.Day~"vaccinated",
                                 earliest==last~"moved"),
-           censored_novacc = case_when(earliest_novacc==as.Date(last_day[subvariant])~"end_period",
-                                       earliest_novacc==last~"moved"))
+           censored_novacc = case_when(earliest_novacc==last_day[subvariant]~"end_period",
+                                       earliest_novacc==last~"moved"), 
+           time_til_earliest = (earliest - first_day[subvariant]) %>% as.numeric(),
+           time_til_earliest_vacc = (earliest_novacc - first_day[subvariant]) %>% as.numeric())
   
+  print((resident_movement_subvar %>% filter(censored!="end_period"))$time_til_earliest %>% hist())
+
   summary_subvar <- resident_movement_subvar %>% 
-    group_by(censored) %>% summarise(prop=n()/nrow(.))
+    group_by(censored) %>% summarise(prop=round(n()/nrow(.),2))
   summary_subvar_novacc <- resident_movement_subvar %>% 
-    group_by(censored_novacc) %>% summarise(prop=n()/nrow(.)) %>% 
+    group_by(censored_novacc) %>% summarise(prop=round(n()/nrow(.),2)) %>% 
     rbind(list(NA, NA, NA))
   
   cbind(subvar=subvariant, n=nrow(resident_movement_subvar)) %>% cbind(summary_subvar, summary_subvar_novacc)
@@ -65,3 +72,5 @@ test <- function(subvariant) {
 
 t <- rbind(test(1), test(2), test(3), test(4))
 t %>% write_csv(here::here("tables/censoring.csv"))
+
+
